7a4cd95c55eedd7e4bd934da73eef1c447a83a05
fdrgsp <federico.gasparoli@gmail.com>
2024-05-08 12:00:06 -0400

    fix: SegmentNeurons in a new process

--- src/napari_micromanager/_segment_neurons.py
+++ src/napari_micromanager/_segment_neurons.py
@@ -1,11 +1,9 @@
-import time
-from collections import deque
-from typing import Generator
+import multiprocessing as mp
+from multiprocessing import Process
 
 import numpy as np
 import useq
 from pymmcore_plus import CMMCorePlus
-from superqt.utils import create_worker
 
 
 class SegmentNeurons:
@@ -15,7 +13,11 @@ class SegmentNeurons:
         self._mmc = mmcore
 
         self._is_running: bool = False
-        self._deck: deque[np.ndarray] = deque()
+
+        self._segmentation_process: Process | None = None
+
+        # Create a multiprocessing Queue
+        self._queue: mp.Queue[np.ndarray | None] = mp.Queue()
 
         self._mmc.mda.events.sequenceStarted.connect(self._on_sequence_started)
         self._mmc.mda.events.frameReady.connect(self._on_frame_ready)
@@ -23,40 +25,49 @@ class SegmentNeurons:
 
     def _on_sequence_started(self, sequence: useq.MDASequence) -> None:
         print("\nSEQUENCE STARTED")
-        self._deck.clear()
         self._is_running = True
 
-        create_worker(
-            self._watch_sequence,
-            _start_thread=True,
-            _connect={
-                "yielded": self._segment_image,
-                "finished": self._segmentation_finished,
-            },
+        # create a separate process for segmentation
+        self._segmentation_process = Process(
+            target=_segmentation_worker, args=(self._queue,)
         )
 
-    def _watch_sequence(self) -> Generator[np.ndarray, None, None]:
-        print("WATCHING SEQUENCE")
-        while self._is_running:
-            if self._deck:
-                yield self._deck.popleft()
-            else:
-                time.sleep(0.1)
+        # start the segmentation process
+        self._segmentation_process.start()
+
+        print("SEGMENTATION WORKER STARTED", self._segmentation_process)
 
     def _on_frame_ready(self, image: np.ndarray, event: useq.MDAEvent) -> None:
         print("FRAME READY", event.index)
-        # if t=0, append to the deck
+        # if t=0, add the image to the queue
         t_index = event.index.get("t")
         if t_index is not None and t_index == 0:
-            self._deck.append(image)
+            # send the image to the segmentation process
+            self._queue.put(image)
 
     def _on_sequence_finished(self, sequence: useq.MDASequence) -> None:
         print("\nSEQUENCE FINISHED")
         self._is_running = False
 
-    def _segment_image(self, image: np.ndarray) -> None:
-        """Segment the image."""
-        print("     SEGMENTING IMAGE", image.shape)
+        # stop the segmentation process
+        self._queue.put(None)
+        if self._segmentation_process is not None:
+            self._segmentation_process.join()
+        self._segmentation_process = None
 
-    def _segmentation_finished(self) -> None:
-        print("     SEGMENTATION FINISHED")
+        print("SEGMENTATION WORKER STOPPED", self._segmentation_process)
+
+
+# this must not be part of the SegmentNeurons class
+def _segmentation_worker(queue: mp.Queue) -> None:
+    """Segmentation worker running in a separate process."""
+    while True:
+        image = queue.get()
+        if image is None:
+            break
+        _segment_image(image)
+
+
+def _segment_image(image: np.ndarray) -> None:
+    """Segment the image."""
+    print("     SEGMENTING IMAGE", image.shape)
